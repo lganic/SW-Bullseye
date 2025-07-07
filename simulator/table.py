8cf17ef82ae2e76579d802336565dc323268e85b
lganic <me@logan-boehm.com>
2025-07-07 13:03:52 -0400

    Added tables

---
+++ simulator/table.py
@@ -0,0 +1,37 @@
+
+
+GUNS = [
+    "Light Autocannon",
+    "Rotary Autocannon",
+    "Heavy Autocannon",
+    "Battle Cannon",
+    "Artillery Cannon",
+    "Bertha Cannon",
+]
+
+MUZZLE_VELOCITY = {
+    "Light Autocannon": 1000,
+    "Rotary Autocannon": 1000,
+    "Heavy Autocannon": 600,
+    "Battle Cannon": 800,
+    "Artillery Cannon": 700,
+    "Bertha Cannon": 600,
+}
+
+PROJECTILE_DRAG = {
+    "Light Autocannon": 0.02,
+    "Rotary Autocannon": 0.01,
+    "Heavy Autocannon": 0.005,
+    "Battle Cannon": 0.002,
+    "Artillery Cannon": 0.001,
+    "Bertha Cannon": 0.0005,
+}
+
+GRAVITY = {
+    "Light Autocannon": 0.5,
+    "Rotary Autocannon": 0.5,
+    "Heavy Autocannon": 0.5,
+    "Battle Cannon": 0.5,
+    "Artillery Cannon": 0.5,
+    "Bertha Cannon": 0.5,
+}
