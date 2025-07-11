2f732c8ecaa0e85543a8a1e552faf9d543242f79
lganic <me@logan-boehm.com>
2025-07-11 14:53:01 -0400

    increased model size

--- neural_net/run_training.py
+++ neural_net/run_training.py
@@ -7,14 +7,14 @@ from loader import BallisticsDataset
 train_dataset = BallisticsDataset("training_files/train.csv")
 val_dataset   = BallisticsDataset("training_files/val.csv")
 
-model = FlexibleMLP(input_size=15, hidden_layers=[24, 48, 24, 12], output_size=2)
+model = FlexibleMLP(input_size=15, hidden_layers=[25, 48, 35, 25, 16, 8], output_size=2)
 
 trained_model = train_model(
     model,
     train_dataset,
     val_dataset,
-    batch_size=128,
-    lr=1e-6,
+    batch_size=1024,
+    lr=1e-5,
     max_epochs=1000000,
     patience=5
 )
